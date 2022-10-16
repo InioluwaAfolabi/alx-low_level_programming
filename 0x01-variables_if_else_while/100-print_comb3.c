ec504ca06eee41d3a22b07ae860266fc2eb55b63
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-10-16 15:24:02 -0700

    commit ten

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a <= 9 && b <= 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
