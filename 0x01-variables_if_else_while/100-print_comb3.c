2a5822ff4072369f91f08a6d189715081d783628
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-10-17 15:00:44 -0700

    updated commit ten

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = a + 1; b < 10; b++)
 		{
 			putchar(a + '0');
 			putchar(b + '0');
-			if (a <= 9 && b <= 9)
+			if (a <= 8 && b <= 9)
 			{
 				putchar(',');
 				putchar(' ');
