bb90fc02aa026cf103d1718999f53085cae5bbf8
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-10-19 07:02:50 -0700

    update commit one

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_alphabet - Print alphabeth in lower case
- * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -10,5 +9,4 @@ void print_alphabet(void)
 	for (t = 'a'; t <= z; t++)
 		_putchar(t);
 	_putchar('\n');
-	return (0);
 }
