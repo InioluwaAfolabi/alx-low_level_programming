2117efc14a6aed6fd0782b976c71ac0e403f0a52
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-10-20 07:29:45 -0700

    commit nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - print 9 times table from 0
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			c = a * b;
+
+			if (c <= 9)
+				_putchar(' ');
+			else
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
