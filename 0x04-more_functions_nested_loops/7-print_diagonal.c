e433530422faa94437bd945297839eed1d0c931f
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-10-21 07:06:01 -0700

    commit sev

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal lines
+ * @n: number of lines
+ */
+
+void print_diagonal(int n)
+{
+	int c, d;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (c = 0; c < n; c++)
+	{
+		for (d = 0; d < n; d++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
