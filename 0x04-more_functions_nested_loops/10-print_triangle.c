3c73298ef22acea28bc2aa47ff5a38ec32bccb2b
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-10-21 10:01:50 -0700

    commit ten

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size: size of triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (a = 1; a <= size; a++)
+	{
+		for (b = size - a; b >= 1; b--)
+		{
+			_putchar(' ');
+		}
+		for (c = 1; c <= size; c++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
