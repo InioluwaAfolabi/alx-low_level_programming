3792d4b2af8cf97f1ed1cc53a20d1043f07cd297
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-10-21 08:33:49 -0700

    commit eight up to date

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,18 @@ void print_square(int size)
 	int c, d;
 
 	if (size <= 0)
-		_putchar('\n');
-	for (c = 0; c < size; c++)
 	{
-		for (d = 0; d < (size); c++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 0; c < size; c++)
+		{
+		for (d = 0; d < (size); d++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
+		}
 	}
 }
