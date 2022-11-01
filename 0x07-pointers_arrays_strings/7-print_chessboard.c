05df0e21a08ada8029358b665fbac90584364418
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-11-01 13:11:09 -0700

    commit six

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - write function to print chessboard
+ * @a: chessboard
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int c, d;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (d = 0; d < 8; d++)
+			_putchar(a[c][d]);
+		_putchar('\n');
+	}
+}
