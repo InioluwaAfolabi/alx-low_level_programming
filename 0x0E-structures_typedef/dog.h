f33a42d924a289d924d7ee766dbf9e9e50989437
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-11-15 18:01:57 -0800

    commit one

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,27 @@
-#ifndef DOG
-#define DOG
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * struct dog - write a class for dug
  * @name: dog name
  * @age: dog age
- * @owner: owner of do
- * Return: 0
+ * @owner: owner of dog
  */
 
 struct dog
 {
 	char *name;
-	int age;
+	float age;
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-typedef struct dog dog_t;
 
 #endif
