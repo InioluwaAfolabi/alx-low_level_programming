642e47b3db96f057b27940758d79386c230362d8
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-12-05 23:43:25 -0800

    commit nil

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - function to read a file and print
+ * @filename: pointer to file
+ * @letters: number of letters to read and print
+ * Return: actual number of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t a, b, c;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	a = open(filename, O_RDONLY);
+	b = read(a, buffer, letters);
+	c = write(STDOUT_FILENO, buffer, b);
+	if (a == -1 || b == -1 || c == -1 || c != b)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(a);
+
+	return (c);
+}
